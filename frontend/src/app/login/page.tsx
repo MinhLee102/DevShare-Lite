b85c554df4d8711b083f39cfeeedbb858c2b4ba0
Minh Lê <leducminh2212@gmail.com>
2025-06-30 20:44:09 +0700

    Added register.tsx page

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import AuthForm, { type FormField } from '@/components/AuthForm';
+import AuthForm, { type FormField } from "@/components/AuthForm";
 
 const loginFields: FormField[] = [
   {
@@ -20,17 +20,17 @@ const loginFields: FormField[] = [
 
 const LoginFooter = (
   <>
-    <div className="mt-6 text-center text-sm">
-      <Link href="/forgot-password">
-        <span className="font-medium text-blue-500 hover:text-blue-600 hover:underline cursor-pointer">
+    <div className= "mt-6 text-center text-sm">
+      <Link href= "/forgot-password">
+        <span className= "font-medium text-blue-500 hover:text-blue-600 hover:underline cursor-pointer">
           Forgot password?
         </span>
       </Link>
     </div>
-    <p className="mt-4 text-center text-sm text-gray-600">
+    <p className= "mt-4 text-center text-sm text-[#AD8989]">
       New to DevShare?{' '}
-      <Link href="/signup">
-        <span className="font-medium text-blue-500 hover:text-blue-600 hover:underline cursor-pointer">
+      <Link href= "/signup">
+        <span className= "font-medium text-blue-500 hover:text-blue-600 hover:underline cursor-pointer">
           Sign Up
         </span>
       </Link>
@@ -48,22 +48,22 @@ export default function LoginPage() {
   };
 
   return (
-    <main className="flex-grow flex flex-col items-center justify-center p-4">
-      <div className="text-center mb-6">
-        <h1 className="text-3xl font-bold text-[#AD8989]">
+    <main className= "flex-grow flex flex-col items-center justify-center p-4">
+      <div className= "text-center mb-6">
+        <h1 className= "text-3xl font-bold text-[#AD8989]">
           Welcome to DevShare Lite!
         </h1>
-        <p className="mt-2 text-sm text-[#AD8989]">
+        <p className= "mt-2 text-sm text-[#AD8989]">
           A Platform for everyone to share about IT related topic
         </p>
       </div>
 
       <AuthForm
-        title="Log In"
-        fields={loginFields}
-        buttonText="Log In"
-        onSubmit={handleLogin}
-        footerContent={LoginFooter}
+        title= "Log In"
+        fields= {loginFields}
+        buttonText= "Log In"
+        onSubmit= {handleLogin}
+        footerContent= {LoginFooter}
       />
     </main>
   );
