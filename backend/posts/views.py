9e3685991f19f79ea8c2a24592bdd41ea9b72b81
Minh Lê <leducminh2212@gmail.com>
2025-07-05 21:59:32 +0700

    Added CRUD for Posts

--- backend/posts/views.py
+++ backend/posts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Post, Tag
+from .serializers import PostSerializer, TagSerializer
+from .permissions import IsAuthorOrReadOnly
 
-# Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.select_related('author').prefetch_related('tags').all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author = self.request.user)
+
+
+class TagViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
